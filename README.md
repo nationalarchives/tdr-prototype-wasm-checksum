1091a3f03b4f3eea9e1542a187ccb4ff12b09b0b
Sam <github@sampalmer.dev>
2019-10-19 17:20:45 +0100

    Removed async from the public function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Checksum calculator
 This package contains a single function, generate_checksum, which takes a file and returns a promise which resolves to the sha256 checksum of this file.
+
+This has been tested using an Intel Pentium N4200 1.1Ghz and this is faster than the Web Crypto digest by around 1 second per 60 Mb of file.
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,20 @@ extern crate wasm_bindgen_futures;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use wasm_bindgen_futures::future_to_promise;
+use web_sys::Blob;
+use web_sys::Response;
 use sha2::{Sha256, Digest};
 
 #[wasm_bindgen]
-pub async fn generate_checksum(blob: web_sys::Blob) -> js_sys::Promise {
+pub fn generate_checksum(blob: Blob) -> js_sys::Promise {
     future_to_promise(checksum(blob))
 }
 
-async fn checksum(blob: web_sys::Blob) -> Result<JsValue, JsValue>{
+async fn checksum(blob: Blob) -> Result<JsValue, JsValue>{
     let mut hasher = Sha256::new();
     for x in (0..blob.size() as i32).step_by(5000) {
         let sliced_blob = blob.slice_with_i32_and_i32(x, x+5000).unwrap();
-        let response = web_sys::Response::new_with_opt_blob(Some(&sliced_blob)).unwrap();
+        let response = Response::new_with_opt_blob(Some(&sliced_blob)).unwrap();
         let array_buffer = response.array_buffer().unwrap();
         let future = JsFuture::from(array_buffer);
         let future_result = future.await?;
