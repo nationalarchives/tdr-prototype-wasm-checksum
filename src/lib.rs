8237d5458b5c8ea603bee0ecbca8605107f142d7
Sam Palmer <github@sampalmer.dev>
2019-10-23 12:21:31 +0100

    Added a callback arg which returns percentage complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,29 @@
 extern crate wasm_bindgen_futures;
+extern crate math;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use wasm_bindgen_futures::future_to_promise;
 use web_sys::Blob;
 use web_sys::Response;
 use sha2::{Sha256, Digest};
+use math::round;
 
 #[wasm_bindgen]
-pub fn generate_checksum(blob: Blob) -> js_sys::Promise {
-    future_to_promise(checksum(blob))
+pub fn generate_checksum(blob: Blob, callback: js_sys::Function) -> js_sys::Promise {
+    future_to_promise(checksum(blob, callback))
 }
 
-async fn checksum(blob: Blob) -> Result<JsValue, JsValue>{
+async fn checksum(blob: Blob, callback: js_sys::Function) -> Result<JsValue, JsValue>{
     let mut hasher = Sha256::new();
-    for x in (0..blob.size() as i32).step_by(5000) {
-        let sliced_blob = blob.slice_with_i32_and_i32(x, x+5000).unwrap();
+    let file_chunk_size: f64 = 10000000.0;
+    let blob_size: f64 = blob.size();
+    let chunk_count = round::ceil(blob_size / file_chunk_size, 0);
+
+    for x in 0..chunk_count as i32 {
+        let start = x as f64 * file_chunk_size;
+        // log_1(&JsValue::from_str(&format!("{}",chunk_count)));
+        let end = start + file_chunk_size;
+        let sliced_blob = blob.slice_with_f64_and_f64(start, end).unwrap();
         let response = Response::new_with_opt_blob(Some(&sliced_blob)).unwrap();
         let array_buffer = response.array_buffer().unwrap();
         let future = JsFuture::from(array_buffer);
@@ -22,7 +31,10 @@ async fn checksum(blob: Blob) -> Result<JsValue, JsValue>{
         let typebuf: js_sys::Uint8Array = js_sys::Uint8Array::new(&future_result);
         let mut body = vec![0; typebuf.length() as usize];        
         typebuf.copy_to(&mut body);
-        hasher.input(&body);       
+        hasher.input(&body);
+        let percentage: f64 = (x as f64 + 1.0) / chunk_count * 100.0;
+        let this = JsValue::NULL;
+        let _ = callback.call1(&this, &JsValue::from_f64(percentage));
     }
     let result = hasher.result();
     let result_string = format!("{:x}", result);
